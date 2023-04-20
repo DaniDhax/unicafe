e0376028eaa27c6b11695683dd8c62d331add28b
DaniDhax <111738016+DaniDhax@users.noreply.github.com>
2023-04-20 15:18:14 +0000

    Part1.Ejercicio 1.6 (2)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const App = () => {
 
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad]= useState(0)
+  
   return (
     <div>
-      <p>Hola react</p>
+      <h1>give feedback</h1>
+      <button>good</button>
+      <button>neutral</button>
+      <button>bad</button>
+      <h1>statistics</h1>
+      <p>good</p>
+      <p>neutral</p>
+      <p>bad</p>
+
     </div>
   )
 }
