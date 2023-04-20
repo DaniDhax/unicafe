d1122aa44310e63e030291fe6c035e07a81ca0ac
DaniDhax <111738016+DaniDhax@users.noreply.github.com>
2023-04-20 15:11:16 +0000

    Part1.Ejercicio 1.6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const App = () => {
+
+  return (
+    <div>
+      <p>Hola react</p>
+    </div>
+  )
+}
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
