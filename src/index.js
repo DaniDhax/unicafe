35cd3cbdc1ab9a8c4cb773760fa988e31ac7d9fa
DaniDhax <DaniDhax@users.noreply.github.com>
2023-04-24 21:05:39 +0200

    Part1.Ejercicio 1.9 (terminado)

--- src/index.js
+++ src/index.js
@@ -1,37 +1,45 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 
-const Statistics = ( {good, neutral, bad}) => {
-
-  return (
-    <div>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average { ((good * 1) + (neutral * 0) + (bad * -1)) / (good + neutral + bad) }</p>
-      <p>positive { (good / (good + neutral + bad)) * 100 }%</p>
-
-    </div>
-  )
-}
+const Statistics = ({ good, neutral, bad }) => {
+  console.log({ good }.good +{ neutral }.neutral + { bad }.bad );
+  if ( { good }.good +{ neutral }.neutral + { bad }.bad === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  } else
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {good + neutral + bad}</p>
+        <p>
+          average {(good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)}
+        </p>
+        <p>positive {(good / (good + neutral + bad)) * 100}%</p>
+      </div>
+    );
+};
 
 const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad]= useState(0)
-  
   const handleGoodClick = () => {
-    setGood (good + 1)
-  }
+    setGood(good + 1);
+  };
   const handleNeutralClick = () => {
-    setNeutral (neutral + 1)
-  }
+    setNeutral(neutral + 1);
+  };
   const handleBadClick = () => {
-    setBad (bad + 1)
-  }
+    setBad(bad + 1);
+  };
 
   return (
     <div>
@@ -41,10 +49,10 @@ const App = () => {
       <button onClick={handleBadClick}>bad</button>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
